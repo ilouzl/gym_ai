48a05ffbdb318b7f6525212d5739d57326273ab5
Liron Ilouz <liron@tapwithus.com>
2019-08-28 17:20:44 +0300

    * q learning based solution for mountain car

--- mountain_car/mountain_car.py
+++ mountain_car/mountain_car.py
@@ -1,106 +1,52 @@
-#!/Users/liron/miniconda3/envs/gym/bin/python
 import gym
-import keras
 import numpy as np
 
-def get_model(input_dim, output_dim=2):
-    model = keras.models.Sequential()
-    model.add(keras.layers.Dense(128, input_dim=input_dim, kernel_initializer='random_uniform'))
-    model.add(keras.layers.Activation('relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(256, kernel_initializer='random_uniform'))
-    model.add(keras.layers.Activation('relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(512, kernel_initializer='random_uniform'))
-    model.add(keras.layers.Activation('relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(256, kernel_initializer='random_uniform'))
-    model.add(keras.layers.Activation('relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(128, kernel_initializer='random_uniform'))
-    model.add(keras.layers.Activation('relu'))
-    model.add(keras.layers.Dropout(0.2))
-    model.add(keras.layers.Dense(output_dim, kernel_initializer='random_uniform'))
-    model.add(keras.layers.Softmax())
-
-    model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-    return model
-
-def extract_data(data):
-    data = np.vstack(data)
-    state = np.vstack(data[:,0])
-    s = np.vstack(data[:,1])
-
-    o = np.vstack(state[:,0])
-    a = np.vstack(state[:,1])
-    r = np.vstack(state[:,2])
-    return {"obs":o, "action":a, "scores":s, "reward":r}
-
 env = gym.make('MountainCar-v0')
-state_history = 2
-n_observation = env.observation_space.sample().size
-n_actions = env.action_space.n
-n_state = n_observation*(1 + state_history)
+
+render = False
+N_QUANTIZATION = 20
+epsilon = 0
+
+lr = 0.3
+discount = 0.8
+
+def quantize_state(s):
+    quantized_state = ((s - env.observation_space.low)/(env.observation_space.high - env.observation_space.low)*N_QUANTIZATION).astype(int)
+    return tuple(quantized_state)
 
 
-model = get_model(input_dim=n_state, output_dim=n_actions)
+q_table_shape = (N_QUANTIZATION, N_QUANTIZATION, env.action_space.n)
+q_table = np.random.rand(N_QUANTIZATION, N_QUANTIZATION, env.action_space.n)
 
-def random_agent(state):
-    return env.action_space.sample()
+q_table_history = []
 
-def agent(state):
-    return np.argmax(model.predict(state.reshape(-1,n_state)))
+for i_episode in range(3000):
+    observation = env.reset()
+    for t in range(1000):
+        quantized_observation = quantize_state(observation)
 
-def play(e, policy, render = False, 
-            max_score=-150, num_of_episodes=1000, 
-            episode_len=500, verbose=0):   
-    episode_memory = []
-    obs_history = np.zeros((1,int(state_history*n_observation))).flatten()
-    for i_episode in range(num_of_episodes):
-        observation = e.reset()
-        score = 0
-        memory = []
-        for t in range(episode_len):
-            if render:
-                e.render()
-            
-            s = np.hstack((obs_history,observation))
-            action = policy(s)
-            state = [s,action]
+        if np.random.random() > epsilon:
+            action = np.argmax(q_table[quantized_observation])
+        else:
+            action = env.action_space.sample()
+        new_observation, reward, done, info = env.step(action)
+        quantized_new_observation = quantize_state(new_observation)
 
-            observation, reward, done, info = env.step(int(action))
-
-            obs_history = np.hstack((obs_history[n_observation:],observation))
-
-            my_score = my_score + observation[0].abs()
-
-            score = score + reward
-            state.append(score)
-            memory.append(state)
-
-            # if done:
-            if observation[1] > 0:
-                if verbose > 0:
-                    print("Episode: {}, steps: {}, score: {}".format(i_episode, t+1, score))
-                if score > max_score:
-                    tmp = [memory, score]
-                    episode_memory.append(tmp)
-                break
-    return episode_memory
-
-
-
-training_data = play(env, random_agent, max_score=-200, num_of_episodes=3000, verbose=1)
-e = extract_data(training_data)
-print("Total %d good episodes" % (len(training_data)))
-print("Score Statistics: max = %d, avg = %d" %(e["scores"].max(), e["scores"].mean()))
-
-model.fit(e["obs"], e["action"],epochs=2)
-
-test_data = play(env, agent, verbose=1, num_of_episodes=100, min_score=0)
-e = extract_data(test_data)
-print("Score Statistics: max = %d, avg = %d" %(e["scores"].max(), e["scores"].mean()))
-if e["scores"].mean() > -110:
-    print("SOLVED!!")
+        if new_observation[0] >= env.goal_position:
+                reward = 2*(1 - t/200)
+        q_table[quantized_observation][action] += lr * (reward + discount * np.max(q_table[quantized_new_observation]) - q_table[quantized_observation][action])
+      
+        if done:
+            if new_observation[0] >= env.goal_position:
+                # q_table[quantized_observation][action] = 0
+                reward = t/200
+                print("Episode {} finished after {} timesteps".format(i_episode,t+1))
+                # render = True
+            else:
+                render = False
+            q_table_history.append(q_table)
+            break
+    
+        if render:
+            env.render()
+        observation = new_observation
